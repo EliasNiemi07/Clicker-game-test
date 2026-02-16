f9c780f4375efcca7387f1a9a8035cb4c92c6db5
EliasNiemi07 <niemielias07@gmail.com>
2026-02-16 14:50:21 +0200

    changes

--- clicker.js
+++ clicker.js
@@ -3,12 +3,15 @@ const button = document.querySelector("#button");
 const cost = document.querySelector("#cost")
 const cpscost = document.querySelector("#cost2")
 const upgrade = document.querySelector("#upgrade")
+const upgrade2 = document.querySelector("#upgrade2")
 count.textContent = "0";
 cost.textContent = "Cost: 30";
 cost2.textContent = "Cost: 100"
 let counter = 0;
 let costnum = 30;
+let costnum2 = 100;
 let clickpower = 1;
+let cps = 0;
 button.addEventListener("click", function () {
     
     counter += clickpower;
@@ -27,7 +30,14 @@ upgrade.addEventListener("click", function () {
         cost.textContent = "Cost: " + Math.floor(costnum)
     }
 });
-setTimeout(() => {
-    counter += 1;
-    count.textContent = counter;
-}, 1000);
+upgrade2.addEventListener("click", function () {
+if (counter >= costnum2) {
+    setInterval(() => {
+        counter -= Math.floor(costnum)
+        cps += 1;
+        counter += cps;
+        count.textContent = counter;
+    }, 1000);
+}
+}
+);
