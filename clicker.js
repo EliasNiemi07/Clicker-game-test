6fc90b11c53cee2c33b8de4c4b49eb57317ac22a
EliasNiemi07 <niemielias07@gmail.com>
2026-03-02 10:39:20 +0200

    added button upgrades

--- clicker.js
+++ clicker.js
@@ -6,22 +6,30 @@ const upgrade = document.querySelector("#upgrade")
 const upgrade2 = document.querySelector("#upgrade2")
 const upgrade3 = document.querySelector("#upgrade3")
 const upgrade3div = document.querySelector("#upgrade3div")
+const upgrade4 = document.querySelector("#upgrade4")
+const upgrade4div = document.querySelector("#upgrade4div")
 const clickdisp = document.querySelector("#click")
 const cpsdisp = document.querySelector("#cps")
+upgrade.textContent = "Click +1";
+upgrade2.textContent = "CPS +1";
 count.textContent = "0";
 cost.textContent = "Cost: 30";
-cost2.textContent = "Cost: 100";
+cost2.textContent = "Cost: 50";
 clickdisp.textContent = "Clickpower: 1";
 cpsdisp.textContent = "CPS: 0"
-let counter = 0;
-let costnum = 30;
-let costnum2 = 100;
+let counter = 4000;
+let costnum = 30; // click upgrade
+let costnum2 = 50; // cps upgrade
+let costnum3 = 200; // upgrade 1
+let costnum4 = 400; // upgrade 2
 let clickpower = 1;
+let clickgiver = 1;
 let cps = 0;
+let cpsgiver = 0;
 
 button.addEventListener("click", function () {
     
-    counter += clickpower;
+    counter += clickpower * clickgiver;
     count.textContent = counter.toFixed(1);
     
 
@@ -35,17 +43,17 @@ upgrade.addEventListener("click", function () {
         costnum *= 1.4;
         costnum = Math.floor(costnum)
         cost.textContent = "Cost: " + costnum
-        clickdisp.textContent = "Clickpower: " + clickpower
-        console.log(clickpower)
+        clickdisp.textContent = "Clickpower: " + clickpower * clickgiver
     }
 });
 upgrade2.addEventListener("click", function () {
 if (counter >= costnum2) {
     counter -= Math.floor(costnum2)
     count.textContent = counter.toFixed(1); 
-    cps += 1;
-    costnum2 += 50;
-    costnum2 *= 1.2;
+    cpsgiver += 0;
+    cps += 1 + cpsgiver;
+    costnum2 += 30;
+    costnum2 *= 1.1;
     costnum2 = Math.floor(costnum2);
     cost2.textContent = "Cost: " + costnum2;
     cpsdisp.textContent = "CPS: " + cps;
@@ -58,6 +66,22 @@ setInterval(() => {
     }, 100);
 
 upgrade3.addEventListener("click", function () {
+    if (counter >= costnum3) {
+    counter -= costnum3
     upgrade3div.classList.add("remove")
+    cps *= 3;
+    cpsgiver += 2;
+    cpsdisp.textContent = "CPS " + cps;
+    upgrade2.textContent = "CPS +3";
+}});
 
-});
+upgrade4.addEventListener("click", function () {
+    if (counter >= costnum4) {
+        counter -= costnum4;
+        upgrade4div.classList.add("remove")
+        clickpower *2;
+        clickgiver += 1;
+        clickdisp.textContent = "Clickpower: " + clickpower * clickgiver
+        upgrade4.textContent = "CPS +2"
+        
+}});
